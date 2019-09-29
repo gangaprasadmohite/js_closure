1e8b167568c00fe161a7817e083f71ec8056d65e
Gangaprasad Mohite <shanky31@localhost.localdomain>
2019-09-29 19:03:30 +0530

    Completed eloquent higher order functions

--- higher-order-functions/eloquent/exercise.js
+++ higher-order-functions/eloquent/exercise.js
@@ -2,10 +2,20 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // Your code here.
+let arr = [];
+arrays.reduce((acc, element, index, array) => {
+	arr = arr.concat(element);
+}, []);
 // → [1, 2, 3, 4, 5, 6]
 
 // Challenge 2. Your own loop
 // Your code here.
+function loop(currentValue, test, update, body) {
+	if (test(currentValue)) {
+		body(currentValue);
+		loop( update(currentValue), test, update, body );
+	} else return;
+}
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
@@ -14,7 +24,12 @@ loop(3, n => n > 0, n => n - 1, console.log);
 
 // Challenge 3. Everything
 function every(array, test) {
-  // Your code here.
+  // Your code goes here.
+  let booleanArr = [];
+  array.reduce((acc, element, index, arr) => {
+	if(test(element) == false) {booleanArr.push(false);} else {booleanArr.push(true);}
+  }, true);
+  if(booleanArr.includes(false)) {return false;} else {return true;};
 }
 
 console.log(every([1, 3, 5], n => n < 10));
@@ -27,6 +42,18 @@ console.log(every([], n => n < 10));
 // Challenge 4. Dominant writing direction
 function dominantDirection(text) {
   // Your code here.
+    let scripts = countBy(text, char => {
+      let script = characterScript(char.codePointAt(0));
+      
+      return script ? script.direction : "none";
+    }).filter(({name}) => name != "none");
+    
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+    
+    return scripts.reduce( (acc, curr) => { 
+      return acc.count < curr.count ? curr : acc;
+    }, {dir: '', count: 0}).name;
 }
 
 console.log(dominantDirection("Hello!"));
